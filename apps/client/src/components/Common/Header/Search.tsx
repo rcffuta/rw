1455cf607076f8becb6af271763dd31d58adf08e
Precious Olusola <preciousolusola16@gmail.com>
2025-05-17 15:56:44 +0100

    Used debounce for smooth search

--- apps/client/src/components/Common/Header/Search.tsx
+++ apps/client/src/components/Common/Header/Search.tsx
@@ -1,27 +1,45 @@
 import { useSearchParams } from "next/navigation";
 import { SearchIcon } from "../Icons";
 import { useNavigate } from "@gamezone/lib";
-import { categoryFilterKey, searchFilterKey } from "@/constants";
+import { categoryFilterKey, searchFilterKey, SHOP } from "@/constants";
+import { useCallback, useEffect, useState } from "react";
 
 export function SearchProduct() {
     const params = useSearchParams();
     const {navigate} = useNavigate();
+    const [query, setQuery] = useState(()=> params.get(searchFilterKey) || "");
+
 
     const filterProducts = (formData: FormData) => {
-        const query = formData.get(searchFilterKey)?.toString() || "";
-        const category = params.get(categoryFilterKey);//formData.get("category")?.toString() || "";
+        // const query = formData.get(searchFilterKey)?.toString() || "";
+        // const category = params.get(categoryFilterKey);//formData.get("category")?.toString() || "";
 
+        goToUrl();
+    }
+    
+    const goToUrl = useCallback(() =>{
         // You can persist this in the URL
-        const search = new URLSearchParams();
+        const category = params.get(categoryFilterKey);
+        const search = new URLSearchParams(Array.from(params.entries()));
+
         if (query) search.set(searchFilterKey, query);
         if (category) search.set(categoryFilterKey, category);
+    
+        navigate(`${SHOP}?${search.toString()}`);
+    }, [query]);
 
-        navigate(`/shop?${search.toString()}`);
-    }
+    useEffect(() => {
+        const handler = setTimeout(goToUrl, 500);
+
+        return () => clearTimeout(handler);
+    }, [query]);
     
     return (
         <form
-            action={filterProducts}
+            onSubmit={(e)=>{
+                e.preventDefault();
+                goToUrl();
+            }}
             className="relative max-w-[333px] sm:min-w-[333px] w-full"
         >
             {/* <!-- divider --> */}
@@ -32,9 +50,11 @@ export function SearchProduct() {
                 type="search"
                 name={searchFilterKey}
                 id="search"
+                value={query}
+                // defaultValue={params.get(searchFilterKey) ?? ""}
+                onChange={(e) => setQuery(e.target.value)}
                 placeholder="Enter product name"
                 autoComplete="off"
-                defaultValue={params.get(searchFilterKey) ?? ""}
                 className="custom-search w-full rounded-r-[5px] bg-gray-1 !border-l-0 border border-gray-3 py-2.5 pl-4 pr-10 outline-none ease-in duration-200"
             />
 
