4bd623a474e984266256cff866bcbed8987ccf26
Precious Olusola <preciousolusola16@gmail.com>
2025-05-17 10:46:31 +0100

    Updated app data

--- packages/lib/data.ts
+++ packages/lib/data.ts
@@ -5,7 +5,7 @@ export const APP_DESCRIPTION = "E-commerce web application that allows users to 
 export const APP_ADMIN_NAME = "GameZone-Admin";
 export const APP_ADMIN_DESCRIPTION = `Manage ${APP_NAME} from behind`;
 
-export const APP_ADMIN_USERNAME = "Waterside Campus, University Drive, Northampton, Northamptonshire, NN1 5PH";
-export const APP_ADMIN_EMAIL = "Waterside Campus, University Drive, Northampton, Northamptonshire, NN1 5PH";
-export const APP_ADMIN_CONTACT = "Waterside Campus, University Drive, Northampton, Northamptonshire, NN1 5PH";
+export const APP_ADMIN_USERNAME = "Funmise";
+export const APP_ADMIN_EMAIL = "fumise@gmail.com";
+export const APP_ADMIN_CONTACT = "+44 7503 043289";
 export const APP_ADMIN_ADDRESS = "Waterside Campus, University Drive, Northampton, Northamptonshire, NN1 5PH";
