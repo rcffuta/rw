f100abcf4b8b84679c830e8c0a06d4918ce1ed31
Precious Olusola <preciousolusola16@gmail.com>
2025-05-21 18:15:23 +0100

    removed generated from project

--- packages/db/prisma/migrations/20250512092141_updated_payment/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Order" DROP CONSTRAINT "Order_paymentId_fkey";
-
--- AlterTable
-ALTER TABLE "Order" ALTER COLUMN "paymentId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Order" ADD CONSTRAINT "Order_paymentId_fkey" FOREIGN KEY ("paymentId") REFERENCES "Payment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
